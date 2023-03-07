71de178d6e6bc9ea44ca229ed47340ae05a26d5c
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-03-07 16:08:13 +0300

    transcation  style added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "./transaction.dart";
+import "package:intl/intl.dart";
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -29,6 +30,8 @@ class MyHomePage extends StatelessWidget {
         amount: 16.53,
         date: DateTime.now())
   ];
+  final titleController = TextEditingController();
+  final amounController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +40,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -47,6 +50,37 @@ class MyHomePage extends StatelessWidget {
                 child: Text("CHART!"),
                 elevation: 5,
               ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                  child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (value) {
+
+                    // },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amounController,
+                    // onChanged: (value) {
+                    //   amountInput = value;
+                    // },
+                  ),
+                  TextButton(
+                    child: Text('Add Transcation'),
+                    style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.purple)),
+                    onPressed: () {
+                      print(titleController.text);
+                    },
+                  )
+                ],
+              )),
             ),
             Column(
               children: transcations.map((tx) {
@@ -58,13 +92,13 @@ class MyHomePage extends StatelessWidget {
                           EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         ),
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -83,7 +117,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat().add_yMMMd().format(tx.date),
                           style: TextStyle(color: Colors.grey),
                         )
                       ],
